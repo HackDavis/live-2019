e2351d0a0eda86ef776e410c15fe607f0133e868
benank <terkolskie@gmail.com>
2020-01-18 07:22:45 -0800

    Remove hover background color for events

--- pages/index.vue
+++ pages/index.vue
@@ -176,10 +176,6 @@ export default {
   }
   transition: 0.1s ease-in-out background-color;
 }
-.schedule-row:hover
-{
-    background: rgba(255, 255, 255, 0.1);
-}
 .schedule-day {
     text-align: center;
   padding: 10px;
